a25e928ee7072c379f2a9b0a8b9f50944eefd0ac
h4x3rotab <h4x3rotab@gmail.com>
2020-11-21 01:42:06 +0800

    git pull before applying changes

--- scripts/publish.sh
+++ scripts/publish.sh
@@ -5,6 +5,11 @@ set -e
 
 printf "\033[0;32mDeploying updates to GitHub...\033[0m\n"
 
+# Update the published site
+cd public
+git pull
+cd ..
+
 # Build the project.
 hugo # if using a theme, replace with `hugo -t <YOURTHEME>`
 
